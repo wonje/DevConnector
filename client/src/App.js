8faf76a247c46140e719b85289b167642c1012d4
wonje <wonjess92@gmail.com>
2020-08-13 01:44:33 -0400

    initial setup for redux

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Landing from './components/layout/Landing';
+import Navbar from './components/layout/Navbar';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
+import './App.css';
+
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
+);
+
+export default App;
